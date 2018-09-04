95afb93a5de91d2a8c4969bd59d8c8a4d6f7bb0b
aaron-gaskin <aaron.gaskin@ufl.edu>
2018-09-04 12:52:16 -0400

    Second

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+  var parsedUrl = url.parse(request.url).pathname;
 
   /*
     Your request handler should send listingData in the JSON format if a GET request 
@@ -16,11 +16,35 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+   
+   
+   //check if the user is looking for /listings
+   if( parsedUrl == '/listings' ){
+        //return data
+    }
+	//otherwise throw a personal 404
+    else{
+		response.writeHead(404, {"Content-type": "text/plain"});
+		response.write("404 Not found!");
+	}
+   
+   response.end();		//close response
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
+	
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+   
+	/*https://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-object-into-server-memory*/
+	if (err) throw err;
+	listingData = JSON.parse(data);		//save data in listingData
+	
+	//define and start the server
+	var server = http.createServer(requestHandler);
+	server.listen(port, function() {
+		//the server is now listening
+	});
 });
