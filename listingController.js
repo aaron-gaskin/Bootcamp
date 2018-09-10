00110c5a859d177fd35fa976eb89a09a06668e67
Aaron <acegaskin@gmail.com>
2018-09-10 16:06:45 -0400

    Can now add and remove listings

--- listingController.js
+++ listingController.js
@@ -7,8 +7,56 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
+	 
+	//add a new listing to the end of Listings
+   $scope.addListing = function() {
+		//create new entry
+		var newEntry = {'code': $scope.code, 'name': $scope.name,
+		'coordinates': { 'latitude': $scope.lat, 'longitude':
+		$scope.longitude}, 'address': $scope.address}
+		
+		//find alphabetical location via for loop
+		var temp;
+		var i;
+		var check = false;
+		for(i=0; i < $scope.listings.length; i++)
+		{
+			temp=$scope.listings[i];
+			if(newEntry.code < temp.code)
+			{
+				console.log("spliced");
+				$scope.listings.splice(i,0,newEntry);	//add new entry
+				
+				//reset values
+				$scope.code = ''
+				$scope.name = ''
+				$scope.lat = ''
+				$scope.longitude = ''
+				$scope.address = ''
+				
+				return;//end code
+			}
+		}
+		//if you get out of the for loop, push it to the end
+		if(!check)
+		{
+			$scope.listings.push(newEntry)	//add new entry
+			
+			//reset values
+			$scope.code = ''
+			$scope.name = ''
+			$scope.lat = ''
+			$scope.longitude = ''
+			$scope.address = ''
+		}
+	};
+	
+	//remove a specific listing based on index location
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index,1);
+	};
+	
+	//defines detailedInfo to the value of a specific listing based on index
     $scope.showDetails = function(index) {
 		$scope.detailedInfo = Listings[index];
 	};
